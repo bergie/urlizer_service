c769d06036cf5518a44018fc70e9864c159bd710
Henri Bergius <henri.bergius@iki.fi>
2012-05-08 10:53:47 +0200

    UI

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<h1>Urlizer service (experimental, testing heroku build pack)</h1>
 <?php
 require 'vendor/midgard/midgardmvc-helper-urlize/interface.php';
-$string = 'älä lyö ääliö ööliä läikkyy';
-echo midgardmvc_helper_urlize::string($string);
+
+if (isset($_GET['urlize'])) {
+    die(midgardmvc_helper_urlize::string($_GET['urlize']));
+}
+?>
+<h1>Urlizer service</h1>
+<form method="GET">
+    <label>
+        String to URLize
+        <input name="urlize" type="text" />
+    </label>
+    <input type="submit" value="URLize" />
+</form>
+
+<p>
+URLize service uses the <a href="http://packagist.org/packages/midgard/midgardmvc-helper-urlize">midgardmvc-helper-urlize</a> library.
+</p>
